cba3be1fd667c5aad4f079608535d35830b57967
Anna <48394404+World-Cup@users.noreply.github.com>
2019-08-19 03:35:31 -0400

    Could not open Product.js in the editor. The editor process exited with an error: spawn code ENOENT.

--- inventory-recording/src/components/Product.js
+++ inventory-recording/src/components/Product.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
-
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../Context';
 export default class Product extends Component {
     render() {
+
+        const {id, title, img, inCart} = this.props.Product
         return (
-            <div>
-                <h3>Hello From Product</h3>
-            </div>
+            <ProductWrapper className = 'col-9 mx-auto col-md-6 col-lg-3'>
+                <div className ='img-container p-5' onClick ={ () =>console.log('image container click btn')}>
+                    <Link to ='/details'>
+                        <img src = {img} alt ="product" className ="card-image-top"/>
+                    </Link> 
+                </div>
+            </ProductWrapper>
         )
     }
-}
+} 
+
+const ProductWrapper = styled.div ``;
